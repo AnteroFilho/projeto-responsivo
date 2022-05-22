c6e72465dfc1824e774b23c7666cf5630ec21898
Antero Filho <anteroa.martinsf@gmail.com>
2022-05-21 22:43:08 -0300

    update-menu

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const iconBar = document.getElementById("menu-bar")
 const menuNav = document.getElementById("menu-nav")
+const openMenu = document.getElementById("open-menu")
+const closeMenu = document.getElementById("close-menu")
 
-iconBar.onclick = abrirMenu;
+openMenu.onclick = abrirMenu;
+closeMenu.onclick = fecharMenu;
 
 function abrirMenu(){
     menuNav.classList.toggle("on")
+    openMenu.classList.toggle("on")
+    if(openMenu.classList = "on"){
+        closeMenu.classList.toggle("on")
+    }
+}
+
+function fecharMenu (){
+    menuNav.classList.toggle("on")
+    openMenu.classList.toggle("on")
 }
